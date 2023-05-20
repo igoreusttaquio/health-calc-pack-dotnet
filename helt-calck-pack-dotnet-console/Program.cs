542f262714c8cc18722433af34d137200c6862c6
Igor <igoreusttaquio@gmail.com>
2023-05-20 14:31:25 -0300

    Projeto de console para testar a implementação

---
+++ helt-calck-pack-dotnet-console/Program.cs
@@ -0,0 +1,29 @@
+﻿using health_calc_pack_dotnet;
+using health_calc_pack_dotnet.MacroNutrientesStrategies;
+using health_calc_pack_dotnet.Models;
+
+MacroNutrientesConext strategy = new();
+strategy.SetStrategy(new GanharMassaMuscularStrategy());
+MacroNutrientesModel resultado = strategy.ExecuteStratery(88.8)!;
+string complemento = "ganho de massa muscular";
+
+string texto = $@"Para uma dieta de {complemento}, consuma:
+{resultado.Carboidratos}g kilo de carboidratos, {resultado.Proteinas}g kilo de proteínas e {resultado.Gorduras}g kilo de gorduras.";
+
+Console.WriteLine(texto);
+
+strategy.SetStrategy(new PerderPesoStrategy());
+resultado = strategy.ExecuteStratery(88.8)!;
+complemento = "perda de peso";
+texto = $@"Para uma dieta de {complemento}, consuma:
+{resultado.Carboidratos}g kilo de carboidratos, {resultado.Proteinas}g kilo de proteínas e {resultado.Gorduras}g kilo de gorduras.";
+Console.WriteLine(texto);
+
+strategy.SetStrategy(new ManterPesoStrategy());
+resultado = strategy.ExecuteStratery(88.8)!;
+complemento = "manter peso";
+texto = $@"Para uma dieta de {complemento}, consuma:
+{resultado.Carboidratos}g kilo de carboidratos, {resultado.Proteinas}g kilo de proteínas e {resultado.Gorduras}g kilo de gorduras.";
+Console.WriteLine(texto);
+
+Console.ReadKey();
