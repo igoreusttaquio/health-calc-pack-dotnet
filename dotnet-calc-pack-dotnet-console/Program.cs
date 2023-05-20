1b19c0d107ba2646b8380fc225b0f6abe29e0b04
Igor <igoreusttaquio@gmail.com>
2023-05-20 14:51:45 -0300

    Renomeando a solution do console

--- dotnet-calc-pack-dotnet-console/Program.cs
+++ dotnet-calc-pack-dotnet-console/Program.cs
@@ -1,17 +1,24 @@
 ﻿using health_calc_pack_dotnet;
+using health_calc_pack_dotnet.Interfaces;
 using health_calc_pack_dotnet.MacroNutrientesStrategies;
 using health_calc_pack_dotnet.Models;
 
+// -------- GANHAR MASSA MUSCULAR
+double altura = 1.70;
+double peso = 88.8;
+IIMC imc = new IMC();
+
 MacroNutrientesConext strategy = new();
 strategy.SetStrategy(new GanharMassaMuscularStrategy());
-MacroNutrientesModel resultado = strategy.ExecuteStratery(88.8)!;
+MacroNutrientesModel resultado = strategy.ExecuteStratery(peso)!;
 string complemento = "ganho de massa muscular";
 
 string texto = $@"Para uma dieta de {complemento}, consuma:
 {resultado.Carboidratos}g kilo de carboidratos, {resultado.Proteinas}g kilo de proteínas e {resultado.Gorduras}g kilo de gorduras.";
 
-Console.WriteLine(texto);
 
+Console.WriteLine(texto);
+// -------- PERDER PESO
 strategy.SetStrategy(new PerderPesoStrategy());
 resultado = strategy.ExecuteStratery(88.8)!;
 complemento = "perda de peso";
@@ -19,11 +26,15 @@ texto = $@"Para uma dieta de {complemento}, consuma:
 {resultado.Carboidratos}g kilo de carboidratos, {resultado.Proteinas}g kilo de proteínas e {resultado.Gorduras}g kilo de gorduras.";
 Console.WriteLine(texto);
 
+// -------- MANTER PESO
 strategy.SetStrategy(new ManterPesoStrategy());
 resultado = strategy.ExecuteStratery(88.8)!;
 complemento = "manter peso";
 texto = $@"Para uma dieta de {complemento}, consuma:
 {resultado.Carboidratos}g kilo de carboidratos, {resultado.Proteinas}g kilo de proteínas e {resultado.Gorduras}g kilo de gorduras.";
 Console.WriteLine(texto);
+
+
+Console.WriteLine($"\nSeu IMC atual é: {imc.Calcular(altura, peso)} e atualmente a categoria é {imc.GetGategoriaIMC(imc.Calcular(altura, peso))}");
 
 Console.ReadKey();
